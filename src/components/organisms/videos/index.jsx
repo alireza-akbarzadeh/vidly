b003b3fbef660fbbc37315dbb89774859262cb2e
Alireza <devtool95@gmail.com>
2022-07-27 21:31:00 +0430

    add React you Tube Video

--- src/components/organisms/videos/index.jsx
+++ src/components/organisms/videos/index.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import { useFetchVideos } from "store/Hook";
+import YouTube from "react-youtube";
 
 const Videos = ({ id }) => {
   const { data, isLoading } = useFetchVideos(id);
   React.useEffect(() => {}, [id]);
+  const onReady = (event) => {
+    event.target.pauseVideo();
+  };
+  const opts = {
+    playerVars: {
+      autoplay: 1,
+    },
+  };
   return (
     !isLoading && (
       <div className='overflow-hidden h-0 pb-[56.25%] relative'>
-        <iframe
-          className='w-full h-full lg:h-[700px] rounded-lg left-0 top-0 absolute'
-          src={`https://www.youtube.com/embed/${data?.results[0]?.key}`}
-          frameBorder='0'
-          allow='autoplay; encrypted-media'
-          allowFullScreen
-          title='video'
+        <YouTube
+          videoId={data?.results[0]?.key}
+          opts={opts}
+          iframeClassName='w-full h-full lg:h-[700px] rounded-lg left-0 top-0 absolute'
+          onReady={onReady}
         />
       </div>
     )
