1d56a1dc2f6627c7dabd8a2a57b863db6dc8b305
Alireza <devtool95@gmail.com>
2022-07-29 12:38:37 +0430

    fiexd search box

--- src/components/organisms/card/index.jsx
+++ src/components/organisms/card/index.jsx
@@ -1,44 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaPlay, FaPlus, FaStar } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { ImInfo } from "react-icons/im";
-const Card = ({ img, id, title, rate, isHoverCard = true, ...rest }) => {
+import { Modal } from "components/molecules";
+const Card = ({
+  img,
+  id,
+  title,
+  rate,
+  className,
+  isHoverCard = true,
+  ...rest
+}) => {
+  const [open, setOpen] = useState(false);
+  const onClose = () => setOpen(false);
   return (
-    <Link to={`/movie/${id}`} {...rest} className={"cursor-pointer mt-6"}>
-      <div className={"mx-1 h-[350px]  relative"}>
-        <LazyLoadImage
-          className={
-            "w-full h-full  object-fill  rounded-tr-2xl rounded-tl-2xl"
-          }
-          alt={title}
-          src={`${process.env.REACT_APP_API_IMG}/${img}`}
-        />
-        <div className='bg-[#1a1a1a] rounded-br-2xl rounded-bl-2xl space-y-4 py-2 px-4'>
-          <div className='inline-flex items-center gap-2'>
-            <FaStar className='text-yellow-400 text-lg' />
-            <span className='text-lg text-gray-100'>{rate.toFixed()}</span>
-          </div>
-          <h3 className='text-gray-300'>{title}</h3>
-          <Link
-            to={`/movie/${id}`}
-            className='bg-[#242323]  flex justify-center gap-3 items-center py-2 rounded-lg'
-          >
-            <FaPlus className='text-sky-600   text-lg' />
-            <span className='text-sky-600 font-semibold'>Watch List</span>
-          </Link>
-          <div className='flex items-center  justify-between'>
-            <div className=' inline-flex  hover:bg-[#242323]  px-3 pt-2  transition-all ease-in-out duration-300 rounded-md items-center gap-2'>
-              <FaPlay className='text-gray-400' />
-              <span className='text-white text-lg'>Trailer</span>
+    <>
+      <div {...rest} className={`cursor-pointer mt-6 ${className}`}>
+        <div className={"mx-1 h-[350px]  relative"}>
+          <LazyLoadImage
+            className={
+              "w-full h-full  object-fill  rounded-tr-2xl rounded-tl-2xl"
+            }
+            alt={title}
+            src={`${process.env.REACT_APP_API_IMG}/${img}`}
+          />
+          <div className='bg-[#1a1a1a] rounded-br-2xl rounded-bl-2xl space-y-4 py-2 px-4'>
+            <div className='inline-flex items-center gap-2'>
+              <FaStar className='text-yellow-400 text-lg' />
+              <span className='text-lg text-gray-100'>{rate.toFixed()}</span>
             </div>
-            <div className='rounded-full px-3 py-3 transition-all ease-in-out duration-300 hover:bg-[#242323]'>
-              <ImInfo className='text-gray-300 text-lg ' />
+            <h3 className='text-gray-300'>{title ? title : "Not exist"}</h3>
+            <Link
+              to={`/movie/${id}`}
+              className='bg-[#242323]  flex justify-center gap-3 items-center py-2 rounded-lg'
+            >
+              <FaPlus className='text-sky-600   text-lg' />
+              <span className='text-sky-600 font-semibold'>Watch List</span>
+            </Link>
+            <div className='flex items-center  justify-between'>
+              <div className=' inline-flex  hover:bg-[#242323]  px-3 pt-2  transition-all ease-in-out duration-300 rounded-md items-center gap-2'>
+                <FaPlay className='text-gray-400' />
+                <span className='text-white text-lg'>Trailer</span>
+              </div>
+              <div
+                onClick={() => setOpen(true)}
+                className='rounded-full px-3 py-3 transition-all ease-in-out duration-300 hover:bg-[#242323]'
+              >
+                <ImInfo className='text-gray-300 text-lg ' />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </Link>
+      {open && (
+        <Modal open={open} onClose={onClose}>
+          test
+        </Modal>
+      )}
+    </>
   );
 };
 
--- src/components/pages/SearchResult.jsx
+++ src/components/pages/SearchResult.jsx
@@ -2,25 +2,33 @@ import { useEffect } from "react";
 import { EffectMovie } from "components/organisms";
 import { useParams } from "react-router-dom";
 import { useSearchMovie } from "store/Hook";
+import Card from "components/organisms/card";
 
 const SearchResult = () => {
   const { slug } = useParams();
   const { data, isLoading, refetch } = useSearchMovie(slug);
-  console.log(data, "data");
   useEffect(() => {
     refetch();
   }, [slug]);
+  console.log(data, "data");
   return (
     !isLoading && (
       <div>
-        <div className='grid grid-cols-3 gap-5 mt-5 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-9 xl:grid-cols-12'>
+        <div className='grid xl:grid-cols-8 sm:grid-cols-4  gap-5 gap-y-52'>
           {data?.results?.length === 0 ? (
             <p className='text-xl md:text-2xl text-gray-200 mt-5 text-center whitespace-pre'>
               Not Result for This Search
             </p>
           ) : (
             data?.results?.map((item) => (
-              <EffectMovie key={item.id} isSearch={true} item={item} />
+              <Card
+                className={"col-span-2"}
+                key={item.id}
+                id={item.id}
+                title={item.original_title}
+                img={item.poster_path}
+                rate={item.vote_average}
+              />
             ))
           )}
         </div>
